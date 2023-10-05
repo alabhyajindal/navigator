150ee3b3014dc3b3883f3eb0cb4a1a3197b5360f
Alabhya Jindal <alabhya10@gmail.com>
2023-10-05 18:14:34 +0530

    Showing person name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def require_signin
+    unless session[:user_id]
+      redirect_to signin_url, alert: 'Please sign in first!'
+    end
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  helper_method :current_user
 end
