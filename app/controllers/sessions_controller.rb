3eb704384d48ba0fd2bbddb9cfd96dfb8841d50c
Alabhya Jindal <alabhya10@gmail.com>
2023-10-05 18:10:52 +0530

    Sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'You are signed in'
+    else
+      flash.now[:alert] = 'Invalid email/password'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
