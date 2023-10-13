d67acd3ab49b43b7314125f5ff3bfffe64c80441
Alabhya Jindal <alabhya10@gmail.com>
2023-10-13 11:08:03 +0530

    Search Mapbox and display search results

--- app/javascript/custom/manage.js
+++ app/javascript/custom/manage.js
@@ -1,7 +1,23 @@
 document.body.style.backgroundColor = 'lightblue'
 
-const test = document.getElementById('test')
+const input = document.getElementById('search-input')
+const search = document.getElementById('search-button')
+const suggestionsContainer = document.getElementById('suggestions')
 
-test.addEventListener('click', (e) => {
-  console.log(e)
+search.addEventListener('click', async (e) => {
+  const query = input.value
+  const res = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=ip&access_token=pk.eyJ1IjoiYWxhYmh5YWppbmRhbCIsImEiOiJjbG5tcmYweW0ya2ZzMmxybDJwYTczZTUzIn0.v3HHB3BcrxYP4TRUJV1Qqw`)
+  const data = await res.json()
+  showSuggestions(data.features)
 })
+
+function showSuggestions(suggestions) {
+  suggestionsContainer.innerHTML = ''
+  const placesNames = suggestions.map(s => {
+    console.log(s.place_name)
+    const p = document.createElement('p')
+    p.textContent = s.place_name
+    suggestionsContainer.appendChild(p)
+  })
+
+}
