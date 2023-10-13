7444c6166cd33f8ce294d5ff6c1917512ac2a905
Alabhya Jindal <alabhya10@gmail.com>
2023-10-13 13:45:45 +0530

    Remove edit and update actions

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -18,19 +18,6 @@ class LocationsController < ApplicationController
     end
   end
 
-  def edit
-    @location = Location.find(params[:id])
-  end
-
-  def update
-    location = Location.find(params[:id])
-    if location.update(location_params)
-      redirect_to root_url, notice: 'Location updated'
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   def destroy
     location = Location.find(params[:id])
     location.destroy
