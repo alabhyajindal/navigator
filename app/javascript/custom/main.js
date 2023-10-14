17339dbf87f076d57e99b6a778975c0f783ce5e4
Alabhya Jindal <alabhya10@gmail.com>
2023-10-15 00:53:34 +0530

    Hack to fix Turbo breaking JS

--- app/javascript/custom/main.js
+++ app/javascript/custom/main.js
@@ -1,22 +1,26 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxhYmh5YWppbmRhbCIsImEiOiJjbG5tcmYweW0ya2ZzMmxybDJwYTczZTUzIn0.v3HHB3BcrxYP4TRUJV1Qqw';
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
-  center: [77.209006, 28.613895], // starting position [lng, lat]
-  zoom: 2, // starting zoom
-});
+document.documentElement.addEventListener("turbo:load", async function (e) {
+  // Hack to run this file where originally intended. Not able to find documentation to keep this scoped when using Turbo
+  if (window.location.pathname === '/') {
+    document.body.style.backgroundColor = 'tomato'
+    const res = await fetch('/api/locations', {method: 'GET'})
+    const data = await res.json()
 
-document.body.style.backgroundColor = 'tomato'
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxhYmh5YWppbmRhbCIsImEiOiJjbG5tcmYweW0ya2ZzMmxybDJwYTczZTUzIn0.v3HHB3BcrxYP4TRUJV1Qqw';
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
+      center: [77.209006, 28.613895], // starting position [lng, lat]
+      zoom: 2, // starting zoom
+    });
 
-const res = await fetch('/api/locations', {method: 'GET'})
-const data = await res.json()
+    function drawPoints(locationsData) {
+      for (const location of locationsData) {
+        const el = document.createElement('div')
+        el.className = 'marker'
+        new mapboxgl.Marker(el).setLngLat(location).addTo(map)
+      }
+    }
 
-function drawPoints(locationsData) {
-  for (const location of locationsData) {
-    const el = document.createElement('div')
-    el.className = 'marker'
-    new mapboxgl.Marker(el).setLngLat(location).addTo(map)
+    drawPoints(data.data)
   }
-}
-
-drawPoints(data.data)
+})
